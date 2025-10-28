c448f8bbfa54a65eefed8adc86ebddc0dbd3587b
Arisa M <aricollegem@gmail.com>
2025-10-27 22:56:57 -0400

    question 5 completed

---
+++ practiceexam2/question5.cpp
@@ -0,0 +1,23 @@
+//convert the following while loop into a for loop 
+
+#include <iostream> 
+
+// int main () {
+//     int j = 1, n; 
+//     while (j <= n){
+//         std::cout << j * j; 
+//         j++; 
+//     }
+// }
+
+//ngl...i dont know ;-; 
+
+int main () {
+    for (int j = 1, n; j <=n; j++){
+        std::cout << j*j;
+    }
+}
+
+//if i get this im da goat 
+//answer: im da goat 
+// basically used my context clues from the last question to use the correct syntax to format this answer
