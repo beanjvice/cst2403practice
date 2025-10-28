13f342c344ba6362400c504df03e2497106c0098
Arisa M <aricollegem@gmail.com>
2025-10-27 23:01:57 -0400

    question 6

---
+++ practiceexam2/question6.cpp
@@ -0,0 +1,14 @@
+//let x = 4, y = 10, min; what is the value for min? 
+
+#include <iostream>
+int main (){
+    int x = 4;
+    int y = 10;
+
+    int min = (x<y)? x-2: y-6;
+    //        |if part|  |else part| 
+}
+
+//prediction: idk cause i have no idea what the ? is 
+// answer: min = 2
+// the ? is a ternary conditional operator, it works like an if else statement in one line 
