40a89c2ebe5a08a2cedb6d0aba33d08e290dbdec
Arisa M <aricollegem@gmail.com>
2025-10-29 20:16:23 -0400

    question 5 completed

---
+++ practice2exam2/question5.cpp
@@ -0,0 +1,16 @@
+//convert this while loop into and equivalent for loop 
+
+#include <iostream>
+
+int main () {
+    // int x = 1;
+    // while(x <= 4) {
+    //     std::cout << x * 2 << " ";
+    //     x++;
+    // }
+
+    for(int x = 1; x<= 4; x * 2){
+        std::cout << x << " ";
+    }
+
+}
