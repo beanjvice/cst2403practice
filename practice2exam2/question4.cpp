428b1fc2b07f7b577f5ecd1cdff6469c51dd60cc
Arisa M <aricollegem@gmail.com>
2025-10-29 20:12:14 -0400

    question 4 completed

---
+++ practice2exam2/question4.cpp
@@ -0,0 +1,29 @@
+// A) what is the output? 
+
+#include <iostream>
+
+int main () {
+    // int i = 1;
+    // while (i < 4)
+    // {
+    //     std::cout << i << " ";
+    //     i++;
+    // }
+
+    // return 0;
+
+    // int j = 1;
+    // do {
+    //     std::cout << j << " ";
+    //     j += 2;
+    // } while (j < 7);
+
+    for (int k = 10; k >= 6; k -= 2)
+    std::cout << k << " ";
+
+}
+
+//A) prediction : 1 2 3 :>!
+//B) prediction : 1 3 5 :>!
+//C) prediciton : 10 8 6 :>!
+
